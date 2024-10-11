7583f2a6e3a377a1ee9b1a503a2a0a3cde91bba8
Vakhushti Metreveli <vakhushti.metreveli@gmail.com>
2024-10-11 10:45:41 +0400

    commit

--- {{cookiecutter.Namespace}}.{{cookiecutter.ProjectName}}/src/integration/AsynchronousAdapter/DependencyInjection.cs
+++ {{cookiecutter.Namespace}}.{{cookiecutter.ProjectName}}/src/integration/AsynchronousAdapter/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace {{cookiecutter.Namespace}}.{{cookiecutter.ProjectName}}.AsynchronousAdapter;
 
