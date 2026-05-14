a193802565a4561b59a771b77a3a773d6d2a900c
Vakhushti Metreveli <vakhushti.metreveli@gmail.com>
2026-05-14 22:36:02 +0400

    chore: update OpenAPI model references in Swagger configuration

--- {{cookiecutter.Namespace}}.{{cookiecutter.ProjectName}}/src/{{cookiecutter.Namespace}}.{{cookiecutter.ProjectName}}.Api/Swagger/ConfigureSwaggerOptions.cs
+++ {{cookiecutter.Namespace}}.{{cookiecutter.ProjectName}}/src/{{cookiecutter.Namespace}}.{{cookiecutter.ProjectName}}.Api/Swagger/ConfigureSwaggerOptions.cs
@@ -1,6 +1,6 @@
 using Asp.Versioning.ApiExplorer;
 using Microsoft.Extensions.Options;
-using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace {{cookiecutter.Namespace}}.{{cookiecutter.ProjectName}}.Api.Swagger;
